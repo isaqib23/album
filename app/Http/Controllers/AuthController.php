8c36e37f72fa395ac7b18ab6bd167da99c182953
Saqib <isaqib23@gmail.com>
2022-02-23 13:56:00 +0400

    dsfdsf

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -64,12 +64,13 @@ class AuthController extends BaseController
         }
 
         // Send Confirmation Email
-        $emailBody = [
+        User::where("id",$user->id)->update(["status" => "active", "email_verified_at" => date("Y-m-d H:i:s")]);
+        /*$emailBody = [
             "name"      => $user->name,
             "url"       => url('/confirmation_email/'.base64_encode($user->id))
         ];
         Mail::to($user->email)->send(new ConfirmationEmail($emailBody));
-
+        */
         return $this->sendResponse([], 'Your account successfully created! Please check your email for confirmation');
     }
 
