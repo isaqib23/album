c0d32a2ff5f6482d109e98fb4c354dc5a4a79818
Saqib <isaqib23@gmail.com>
2022-03-19 18:50:39 +0400

    dfdfd

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Entities\Friend;
 use App\Entities\Notification;
+use App\Repositories\FriendRepositoryEloquent;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -20,17 +22,24 @@ class NotificationsController extends BaseController
      * @var NotificationRepository
      */
     protected $repository;
+    /**
+     * @var FriendRepositoryEloquent
+     */
+    private $friendRepositoryEloquent;
 
     /**
      * NotificationsController constructor.
      *
      * @param NotificationRepository $repository
+     * @param FriendRepositoryEloquent $friendRepositoryEloquent
      */
     public function __construct(
-        NotificationRepository $repository
+        NotificationRepository $repository,
+        FriendRepositoryEloquent $friendRepositoryEloquent
     )
     {
         $this->repository = $repository;
+        $this->friendRepositoryEloquent = $friendRepositoryEloquent;
     }
 
     /**
@@ -53,7 +62,14 @@ class NotificationsController extends BaseController
         ]);
 
         $result = $this->repository->update(["status" => $request->input("status")],$request->input("notify_id"));
-        dd($result);
+        if($result->type == "friend_request"){
+            Friend::where([
+                "user_id"   => $result->receiver,
+                "invited_by"   => $result->sender,
+            ])->update(["status" => $request->input("status")]);
+        }/*elseif ($result->type == "album_invitation"){
+
+        }*/
         return $this->sendResponse($result,"Notification updated");
     }
 }
