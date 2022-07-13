093d3107c42c4c188e177a542c0a896577c1c8a3
Saqib <isaqib23@gmail.com>
2022-07-13 15:09:13 +0400

    dfds

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -51,7 +51,7 @@ class NotificationsController extends BaseController
     public function index(Request $request){
         $response = Notification::orderBy("id","desc")->where("status","sent")->get();
         $user = auth()->user()->device_UUID;
-        sendPushNotification($user,'Test');
+        sendPushNotification($user,'5e2a485c-df58-42e1-81d4-2b70e37fa964');
         return $this->sendResponse($response,"Notification updated");
     }
 
