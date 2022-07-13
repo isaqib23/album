fa1c70411e92c0fa0c81a9e7c8f6b12d21c8d390
Saqib <isaqib23@gmail.com>
2022-07-13 15:07:34 +0400

    dfds

--- app/helpers.php
+++ app/helpers.php
@@ -2,7 +2,11 @@
 
 if(!function_exists("sendPushNotification")){
     function sendPushNotification($userId,$template_id){
-        $oneSignal = new Berkayk\OneSignal\OneSignalClient();
+        $oneSignal = new Berkayk\OneSignal\OneSignalClient(
+            env('ONESIGNAL_APP_ID'),
+            env('ONESIGNAL_REST_API_KEY'),
+            env('USER_AUTH_KEY')
+        );
         $params = [];
         $params['include_player_ids'] = [$userId];
         $params['content_available'] = true;
