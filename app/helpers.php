68f7967f1b31cfa6db7cbfa6b5840f1f970dcf21
saqib.rao <saqib.rao@schoolvoice.com>
2022-09-27 12:27:25 +0400

    sdfdf

--- app/helpers.php
+++ app/helpers.php
@@ -2,16 +2,18 @@
 
 if(!function_exists("sendPushNotification")){
     function sendPushNotification($userId,$template_id){
-        $oneSignal = new Berkayk\OneSignal\OneSignalClient(
-            env('ONESIGNAL_APP_ID'),
-            env('ONESIGNAL_REST_API_KEY'),
-            env('USER_AUTH_KEY')
-        );
-        $params = [];
-        $params['include_player_ids'] = [$userId];
-        $params['content_available'] = true;
-        $params['template_id'] = $template_id;
-        $oneSignal->setParam('priority', 10)->sendNotificationCustom($params);
-        return true;
+        if(!is_null($userId)) {
+            $oneSignal = new Berkayk\OneSignal\OneSignalClient(
+                env('ONESIGNAL_APP_ID'),
+                env('ONESIGNAL_REST_API_KEY'),
+                env('USER_AUTH_KEY')
+            );
+            $params = [];
+            $params['include_player_ids'] = [$userId];
+            $params['content_available'] = true;
+            $params['template_id'] = $template_id;
+            $oneSignal->setParam('priority', 10)->sendNotificationCustom($params);
+            return true;
+        }
     }
 }
