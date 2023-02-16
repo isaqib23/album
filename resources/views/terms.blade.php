3b3255fa3b1f1e25c0c1dce1a85f73fd344e29f2
saqib.rao <saqib.rao@schoolvoice.com>
2023-02-16 16:51:45 +0400

    dsfdsf'

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -3,52 +3,55 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title></title>
 </head>
-<body>
-<p><strong><span style="font-size: 22px;">Personal *********** Collection</span></strong></p>
-<p>The app **** *** collect any personal information ***** **** the user&apos;s birthday, which is optional *** not mandatory to use the app.&nbsp;</p>
+<body style="width: 80%; margin: 0 auto">
+<p><strong><span style="font-size: 22px;">Personal Information Collection</span></strong></p>
+<p>The app does not collect any personal information apart from the user's birthday, which is optional and not mandatory to use the app. </p>
 <p><br></p>
-<p><strong><span style="font-size: 22px;">Data Protection Commitment</span></strong></p>
-<p>Secret Albums is committed to protecting the ******* of all ********** parties. Secret Albums collects, processes, stores, and uses data only for ********** business purposes. Information containing data about employees, business partners, and other interested parties are treated as confidential, are stored carefully, and disclosed only to authorized persons. Secret Albums respects and ****** ** all ********** ***** data privacy laws, ********* the EU GDPR.&nbsp;</p>
+<p><strong><span style="font-size: 22px;">Data  Protection Commitment</span></strong></p>
+<p>Secret Albums is committed to protecting the privacy of all interested parties. Secret Albums collects, processes, stores, and uses data only for legitimate business purposes. Information containing data about employees, business partners, and other interested parties are treated as confidential, are stored carefully, and disclosed only to authorized persons. Secret Albums respects and abides by all applicable local data privacy laws, including the EU GDPR.</p>
 <p><br></p>
 <p><strong><span style="font-size: 22px;">Personal Data Definition</span></strong></p>
-<p>Personal data related to the ****** Albums app may include, but is not limited to, the name, address, telephone number, photographs, date of birth, and e-mail address of app users, or any other personal data that has been provided or generated to facilitate the usage of Secret Albums.&nbsp;</p>
+<p>Personal data related to the Secret Albums app may include, but is not limited to, the name, address, telephone number, photographs, date of birth, and e-mail address of app users, or any other personal data that has been provided or generated to facilitate the usage of Secret Albums. </p>
 <p><br></p>
 <p><span style="font-size: 22px;"><strong>Data Subject&rsquo;s Rights</strong></span></p>
-<p>Secret Albums employees consider the data subject&rsquo;s ****** when processing any personal data, ensuring that:&nbsp;</p>
-<p>Data subjects are kept fully informed of their rights, the personal data Secret Albums ***** about them, *** that personal data is used, and how long Secret Albums will **** that ******** data.&nbsp;</p>
-<p>Data subjects are given control over ***** personal data held by Secret Albums including&nbsp;</p>
+<p>Secret Albums employees consider the data subject’s rights when processing any personal data, ensuring that: </p>
+<p>Data subjects are kept fully informed of their rights, the personal data Secret Albums holds about them, how that personal data is used, and how long Secret Albums will hold that personal data. </p>
+<p>Data subjects are given control over their personal data held by Secret Albums including</p>
 <p>(1) the right to have incorrect data rectified,</p>
-<p>(2) the right ** request that their personal data be deleted ** otherwise disposed of,</p>
-<p>(3) *** right to restrict the Company&rsquo;s use of their personal data,&nbsp;</p>
-<p>(4) the ***** to data portability, and&nbsp;</p>
+<p>(2) the right to request that their personal data be deleted or otherwise disposed of,</p>
+<p>(3) the right to restrict the Company&rsquo;s use of their personal data,&nbsp;</p>
+<p>(4) the right to data portability, and</p>
 <p>(5) further rights relating to automated decision-making and profiling.</p>
 <p><br></p>
-<p><span style="font-size: 22px;"><strong>Secret Albums **** of Conduct</strong></span></p>
-<p>Render information ethically: **** *** are adding personal data associated with you and/or third parties (e.g., names, phone numbers, photographs, videos, and other details) to the Secret Albums app, make sure you consider all the data protection principles.&nbsp;</p>
+<p><span style="font-size: 22px;"><strong>Secret Albums Code of Conduct </strong></span></p>
+<p>Render information ethically: When you are adding personal data associated with you and/or third parties (e.g., names, phone numbers, photographs, videos, and other details) to the Secret Albums app, make sure you consider all the data protection principles</p>
 <ul>
-    <li>Control information flow: Some ** the personal information *** added to *** Secret Albums app may be visible to unknown parties, make sure you are not ********* any legal or *********** obligation that&rsquo;s been agreed.&nbsp;</li>
-    <li>Personal data deletion: Secret Albums does not automatically ****** any personal data. This means that users should request the removal ** ******** data ** a timely manner, considering the operational and regulatory requirements. User ************** The user is solely responsible for the content they upload ** the app, and *** app is not liable for *** misuse or infringement of intellectual property rights related to such content.</li>
+    <li>Control information flow: Some of the personal information you added to the Secret Albums app may be visible to unknown parties, make sure you are not breaching any legal or contractual obligation that’s been agreed</li>
+    <li>Personal data deletion: Secret Albums does not automatically delete any personal data. This means that users should request the removal of personal data in a timely manner, considering the operational and regulatory requirements. </li>
+    <li>User Responsibility: The user is solely responsible for the content they upload to the app, and the app is not liable for any misuse or infringement of intellectual property rights related to such content.
+    </li>
 </ul>
 <p>&nbsp;</p>
-<p><span style="font-size: 22px;"><strong>Compatibility and Age Requirement&nbsp;</strong></span></p>
-<p>The app is ********* on Apple App Store and is compatible with Apple devices. The user must ** at least 18 years old to use the app. ** the **** is below 18 years of age, they must use the app under adult supervision.&nbsp;</p>
+<p><span style="font-size: 22px;"><strong>Compatibility and Age Requirement </strong></span></p>
+<p>The app is available on Apple App Store and is compatible with Apple devices. The user must be at least 18 years old to use the app. If the user is below 18 years of age, they must use the app under adult supervision. </p>
 <p>&nbsp;</p>
 <p><span style="font-size: 22px;"><strong>Termination of Account&nbsp;</strong></span></p>
-<p>The app reserves the right to terminate a user&apos;s account if they violate any of the terms and conditions mentioned herein.&nbsp;</p>
+<p>The app reserves the right to terminate a user's account if they violate any of the terms and conditions mentioned herein.</p>
 <p>&nbsp;</p>
 <p><span style="font-size: 22px;"><strong>Promotional Messages</strong></span></p>
-<p>The *** may from time to time send promotional ******** or updates to the user via the app or email.&nbsp;</p>
+<p>The app may from time to time send promotional messages or updates to the user via the app or email. </p>
 <p>&nbsp;</p>
 <p><span style="font-size: 22px;"><strong>Cookies</strong></span></p>
-<p>*** app may use ******* to ******* the user experience, but the user *** the right to disable ******* if they so choose.&nbsp;</p>
+<p>The app may use cookies to enhance the user experience, but the user has the right to disable cookies if they so choose.</p>
 <p>&nbsp;</p>
-<p><span style="font-size: 22px;"><strong>Limitation of Liability&nbsp;</strong></span></p>
-<p>The app is not *********** for any loss or damage caused by the use of the app, including but not limited to loss of data, revenue or profits, or any ***** consequential or incidental damages.&nbsp;</p>
+<p><span style="font-size: 22px;"><strong>Limitation of Liability </strong></span></p>
+<p>The app is not responsible for any loss or damage caused by the use of the app, including but not limited to loss of data, revenue or profits, or any other consequential or incidental damages.</p>
 <p>&nbsp;</p>
 <p><span style="font-size: 22px;"><strong>Modification of Terms and Conditions</strong></span></p>
-<p>&nbsp;The app reserves the right to ****** or update these terms and conditions at any time without notice.&nbsp;</p>
+<p>The app reserves the right to modify or update these terms and conditions at any time without notice. </p>
 </body>
 </html>
